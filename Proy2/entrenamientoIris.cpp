1473011247d9137b561f4954e89a3d945e3d0e56
UannaFF <anna.gomez.op@gmail.com>
2014-10-16 18:52:56 -0430

    D

--- Proy2/entrenamientoIris.cpp
+++ Proy2/entrenamientoIris.cpp
@@ -4,20 +4,24 @@
 using namespace std;
 
 int main(){
-
-	ifstream infile("berzek.data");
-	for(string line; getline(source, line);)   
+	int* esperados;
+	ifstream infile("dat.data");
+	for(string line; getline(infile, line);)   
 	{
 	    istringstream in(line);
-
-	    if(type == "")
-	    {
-	        float x, y, z, t;
-	        in >> x >> y >> z >> t;
-	    }
+	    float x, y, z, t;
+	    in >> x >> y >> z >> t;
 
 	    string type;
 	    in >> type;
+
+	    if(type == "Iris-setosa"){
+
+	    } else if(type == "Iris-versicolor") {
+
+	    } else {
+
+	    }
 
 	}
 
