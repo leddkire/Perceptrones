608753e1007b19a5062ac35faf948cae1f272741
UannaFF <anna.gomez.op@gmail.com>
2014-11-27 14:05:55 -0430

    Red neuronal adaptada para imagenes

--- ProyFinal/entrenamientoIrisClases.cpp
+++ ProyFinal/entrenamientoIrisClases.cpp
@@ -10,8 +10,8 @@
 using namespace std;
 
 void prueba_Clases(Capa* red, int numEntradas, int numSalidas, int numCapas, string nombreArchivo){
-	double entradas[4];
-	double test[3];
+	double entradas[32*44];
+	double test[4];
 	double* resultado;
 	int aciertos =0;
 	int lineas = 0;
@@ -20,45 +20,58 @@ void prueba_Clases(Capa* red, int numEntradas, int numSalidas, int numCapas, str
 	resultadosPrueba.open(nombreArchivo);
 	ifstream datosPrueba;
 	string linea;
-	double esp[3][3] = {{1,0,0},{0,1,0}, {0,0,1}};
-	datosPrueba.open("./berzek.data");
+	double esp[4][4] = {{1,0,0,0},{0,1,0,0}, {0,0,1,0}, {0,0,0,1}};
+	datosPrueba.open("pixel_test.txt");
 	while(getline(datosPrueba,linea)){
 
 		istringstream entrada(linea);
 		char ch;
 		for(int j = 0 ; j < numEntradas; j++){
 			entrada >> coordenadas[j];
-			entrada >> ch;
+			//entrada >> ch;
 		}
 		string tipo;
 		entrada >> tipo;
 		int tipoEsp =0;
-		if(tipo == "Iris-setosa"){
+		cout << tipo << "\n";
+		if(tipo == "happy"){
 			tipoEsp =0;
-		}else if(tipo == "Iris-versicolor"){
+		}else if(tipo == "angry"){
 			tipoEsp =1;
-		}else if(tipo == "Iris-virginica"){
+		}else if(tipo == "sad"){
 			tipoEsp =2;
+		}else if(tipo == "surprised"){
+			tipoEsp =3;
 		}
 		for(int j = 0; j <numSalidas; j++){			
 			test[j] = esp[tipoEsp][j];
 		}
 		resultado = red -> calcularSalida(coordenadas, numEntradas,numCapas,red);
-		resultadosPrueba << resultado[0] << " " <<  resultado[1]<< " " <<  resultado[2] << "\n";
+		resultadosPrueba << resultado[0] << " " <<  resultado[1]<< " " <<  resultado[2] << " " << resultado[3] << "\n";
 		if(resultado[0] >= 0.5 && test[0] == 1){
 			if(resultado[1] < 0.5 && test[1] ==0){
 				if(resultado[2]<0.5 && test[2] == 0){
-					aciertos++;
+					if(resultado[3]<0.5 && test[3] == 0){
+						aciertos++;
+					}
 				}
 			}
     	}else if(resultado[0] < 0.5 && test[0]==0){
     		if(resultado[1] >= 0.5 && test[1] ==1){
 				if(resultado[2]<0.5 && test[2] == 0){
-					aciertos++;
+					if(resultado[3]<0.5 && test[3] == 0){
+						aciertos++;
+					}
 				}
 			}else if(resultado[1] < 0.5 && test[1] == 0){
 				if(resultado[2]>=0.5 && test[2] == 1){
-					aciertos++;
+					if(resultado[3]<0.5 && test[3] == 0){
+						aciertos++;
+					}
+				}else if(resultado[2]<0.5 && test[2] == 0){
+					if(resultado[3]>=0.5 && test[3] == 1){
+						aciertos++;
+					}
 				}
 			}
 
@@ -74,29 +87,29 @@ void prueba_Clases(Capa* red, int numEntradas, int numSalidas, int numCapas, str
 
 int main(){
 	//Se inicializa la capa intermedia con el valor provisto.
-	int numEntradas = 4;
+	int numEntradas = 32*44;
 	int numCapas = 2;
-	int numSalidas = 3;
+	int numSalidas = 4;
 	double eta = 0.1;
-	ifstream entradas [5];
-	entradas[0].open("iris50.txt");
-	entradas[1].open("iris60.txt");
-	entradas[2].open("iris70.txt");
-	entradas[3].open("iris80.txt");
-	entradas[4].open("iris90.txt");
-	int numArchivos = 5;
+	ifstream entradas [1];
+	entradas[0].open("pixel_input.txt");
+	//entradas[1].open("iris60.txt");
+	//entradas[2].open("iris70.txt");
+	//entradas[3].open("iris80.txt");
+	//entradas[4].open("iris90.txt");
+	int numArchivos = 1;
 	ofstream errores;
 	UnidadSigmoidal* salida = new UnidadSigmoidal[numSalidas];
 	double* coordenadas = new double[numEntradas];
 	double* test = new double[numSalidas];
 	//Pidiendo numero de neuronas que habra en la capa intermedia.
-	
+	int numeroNeuronasInter = 5;
 
-	for(int numeroNeuronasInter = 4; numeroNeuronasInter <=10; numeroNeuronasInter++){
+	//for(int numeroNeuronasInter = 4; numeroNeuronasInter <=10; numeroNeuronasInter++){
 		
 		UnidadSigmoidal* intermedia = new UnidadSigmoidal[numeroNeuronasInter];
 		stringstream nombreDir;
-		nombreDir << "ejercicio3_" << numeroNeuronasInter;
+		nombreDir << "ResultadosRed" << numeroNeuronasInter;
 		string temp = nombreDir.str();
 		const char* nDir = temp.c_str();
 		mkdir(nDir,S_IRWXU);
@@ -129,7 +142,7 @@ int main(){
 			}
 
 			stringstream nombreArchivo;
-			nombreArchivo << "./"<< "ejercicio3_" << numeroNeuronasInter << "/" << "errores_clases_porcentaje_" << 50+(10*i);
+			nombreArchivo << "./"<< "ResultadosRed" << numeroNeuronasInter << "/" << "errores_clases_porcentaje_" << 50+(10*i);
 			ofstream errores;
 			errores.open(nombreArchivo.str());
 			
@@ -139,7 +152,7 @@ int main(){
 			int iteraciones_maximas=10000;
 			int iteracion = 0;
 			int lineas = 0;
-			double esp[3][3] = {{1,0,0},{0,1,0}, {0,0,1}};
+			double esp[4][4] = {{1,0,0,0},{0,1,0,0}, {0,0,1,0}, {0,0,0,1}};
 			do{
 			iteracion++;
 			error_global =0;
@@ -150,19 +163,25 @@ int main(){
 
 					for(int j = 0 ; j < numEntradas; j++){
 						entrada >> coordenadas[j];
-						entrada >> ch;
+						//cout << coordenadas[j] << " ";
+						//entrada >> ch;
 					}
+					
 					
 					string tipo;
 					entrada >> tipo;
+					//cout << "tipo "<< tipo << "\n";
 					int tipoEsp =0;
-					if(tipo == "Iris-setosa"){
+					if(tipo == "happy"){
 						tipoEsp =0;
-					}else if(tipo == "Iris-versicolor"){
+					}else if(tipo == "angry"){
 						tipoEsp =1;
-					}else if(tipo == "Iris-virginica"){
+					}else if(tipo == "sad"){
 						tipoEsp =2;
+					}else if(tipo == "surprised"){
+						tipoEsp =3;
 					}
+					
 					for(int j = 0; j <numSalidas; j++){			
 						test[j] = esp[tipoEsp][j];
 					}
@@ -176,13 +195,13 @@ int main(){
 			}while(error_global >= 0.0001 && iteracion < iteraciones_maximas );
 			errores.close();
 			stringstream nombrePrueba;
-			nombrePrueba << "./ejercicio3_"<< numeroNeuronasInter << "/" << "resultados_conjunto_clases" << i;
+			nombrePrueba << "./ResultadosRed"<< numeroNeuronasInter << "/" << "resultados_conjunto_clases" << i;
 			prueba_Clases(red,numEntradas,numSalidas,numCapas,nombrePrueba.str());
 		}
 		cout << "Se Termino de entrenar \n";
 
 		
-	}
+	//}
 
 
 }
